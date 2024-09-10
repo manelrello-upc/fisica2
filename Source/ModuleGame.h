e839cdc18f9db201a481d4504bf85c4bc1c5e6a5
Manuel Rello <manuel.rello@upc.edu>
2024-09-10 16:25:19 +0200

    Added TODOs for students

--- Source/ModuleGame.h
+++ Source/ModuleGame.h
@@ -25,4 +25,17 @@ public:
 public:
 
 	
+	// TODO 1: Every second, create a circle at the leftmost side of the screen at a random height. These circles should be destroyed after 10 seconds.
+	// TIP: You can check (and maybe reuse) some previous Handout...
+	
+	// TODO 2: Apply an horizontal Force to each circle so it crosses the screen with constant acceleration. (NO GRAVITY to the bottom of the screen)
+
+	// TODO 3: Assign a random weight to each circle (from 1Kg to 100Kg), and make them bigger (or smaller) depending on their weight.
+
+	// TODO 4: Apply a friction to these circles (as if the game had a top view) while they cross the screen. 
+
+	// TODO 5: With each left click, increase the STATIC friction coeficient.
+
+	// TODO 6: With each right click, increase the DYNAMIC friction coeficient.
+
 };
