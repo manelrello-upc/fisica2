d650b54e41796a7e7f344a7b8fd6d015d843bf3f
Christian Martínez de la Rosa <36189169+christt105@users.noreply.github.com>
2024-10-27 19:08:52 +0100

    fix

--- Source/ModuleGame.cpp
+++ Source/ModuleGame.cpp
@@ -95,7 +95,7 @@ public:
 
 class Car : public Box {
 public:
-	Car(ModulePhysics* physics, int _x, int _y, Module* _listener, Texture2D _texture) : Box(physics, _x, _y, 26, 43, _listener, _texture, PhysicCategory::CAR, PhysicCategory::DEFAULT | PhysicCategory::CAR, PhysicGroup::LAND) {
+	Car(ModulePhysics* physics, int _x, int _y, Module* _listener, Texture2D _texture) : Box(physics, _x, _y, 26, 43, _listener, _texture, PhysicCategory::CAR, PhysicCategory::DEFAULT, PhysicGroup::LAND) {
 		body->body->ApplyForce(b2Vec2(0.0f, 100.f), body->body->GetWorldCenter(), true);
 	}
 };
